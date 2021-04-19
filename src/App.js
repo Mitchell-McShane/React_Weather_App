2a4c126b1d2dc81caed9ace53c98492a26566abe
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-04-19 10:54:37 +0100

    Added api key from openweather site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import React from 'react';
+
+const api = {
+  key: "b9035784c4dfc6594632b6a8de132a10",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
