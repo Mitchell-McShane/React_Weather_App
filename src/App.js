a3a89766a39815452056c468ea22fd2b4edb251d
Riph Kitsune <mitchell.mcshane1987@gmail.com>
2021-04-19 11:33:38 +0100

    Created Date Builder which should show off current date

--- src/App.js
+++ src/App.js
@@ -7,15 +7,32 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
       <main>
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Scotland, GB</div>
+          <div className="location-date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
